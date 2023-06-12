785c3198bbc5946b61dfb1b5abbd9e5b75c408b4
Rafal Praitnickas <115026306+Praitnickas-Rafal@users.noreply.github.com>
2023-06-12 12:28:57 +0300

    Add files via upload

---
+++ Zadanie2/Zadanie2/app/src/main/java/com/example/zadanie2/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.zadanie2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.View;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void disable(View v){
+        v.setEnabled(false);
+    }
+}
